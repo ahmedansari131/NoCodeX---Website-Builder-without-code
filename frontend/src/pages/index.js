d06854aded84e3c86fd20f989e3ef903059f8c23
Ahmed Ansari <ansarimohammadahmed49@gmail.com>
2023-09-30 12:16:40 +0530

    Export for verification page added

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,9 +1,11 @@
 import Login from "./login/Login"
 import Landing from "./Landing"
 import Signup from "./signup/Signup"
+import EmailVerification from "./verification/EmailVerification"
 
 export {
     Login,
     Landing,
     Signup,
+    EmailVerification,
 }
