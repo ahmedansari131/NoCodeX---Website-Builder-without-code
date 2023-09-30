517a1a6b1d4cacc7e6fe937f8d66ec0f3d14d88a
Ahmed Ansari <ansarimohammadahmed49@gmail.com>
2023-09-30 12:15:04 +0530

    Width added

--- frontend/src/components/container/Container.jsx
+++ frontend/src/components/container/Container.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Container = ({ children }) => {
-  return <div className="w-full mx-auto">{children}</div>;
+  return <div className="w-full mx-auto h-full">{children}</div>;
 };
 
 export default Container;
