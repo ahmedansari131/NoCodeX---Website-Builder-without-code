84073910b855e305dd18e6cb83caa4aa1fa91ed5
Ahmed Ansari <ansarimohammadahmed49@gmail.com>
2023-09-30 12:18:01 +0530

    Width changed of input field

--- frontend/src/components/input/Input.jsx
+++ frontend/src/components/input/Input.jsx
@@ -13,7 +13,7 @@ const Input = forwardRef(function Input(
         </label>
       )}
 
-      <input type={type} className={`outline-none text-md px-3 pr-10 py-2 rounded-md w-96 focus:border-gray-500 text-gray-100 h-10 bg-gray-700 border border-gray-600 ${className}`} ref={ref} id={id} {...props}  />
+      <input type={type} className={`outline-none text-md px-3 pr-10 py-2 rounded-md w-full focus:border-gray-500 text-gray-100 h-10 bg-gray-700 border border-gray-600 ${className}`} ref={ref} id={id} {...props}  />
     </div>
   );
 });
