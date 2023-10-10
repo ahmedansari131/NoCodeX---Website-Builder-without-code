c4cdcb68dd8d3a4d833855d0c1de7d00258a4e3c
Ahmed Ansari <ansarimohammadahmed49@gmail.com>
2023-10-10 21:02:44 +0530

    Private routes configured

--- frontend/src/components/Private/PrivateRoutes.jsx
+++ frontend/src/components/Private/PrivateRoutes.jsx
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Navigate, Outlet } from "react-router-dom";
+import { EmailVerification, ProfileSetup } from "../../pages";
 
-const PrivateRoutes = (props) => {
-  const { VerifyEmail } = props;
-  const isRegister = JSON.parse(localStorage.getItem("registration"));
+const PrivateRoutes = () => {
+  const isRegistered = localStorage.getItem("registration");
+  const isLoggedIn = useSelector((state) => state.auth.status);
 
-  return <>{isRegister["isRegisterSuccess"] && <VerifyEmail />}</>;
+  return <>{isLoggedIn || isRegistered ? <Outlet /> : <Navigate to="/" />}</>;
 };
 
 export default PrivateRoutes;
