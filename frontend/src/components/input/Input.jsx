209b1c9b1e32dc24c1788ff0609f97a487f1fca3
Ahmed Ansari <ansarimohammadahmed49@gmail.com>
2023-10-10 21:05:21 +0530

    Input component created

--- frontend/src/components/input/Input.jsx
+++ frontend/src/components/input/Input.jsx
@@ -1,20 +1,30 @@
 import React, { forwardRef, useId } from "react";
+import Error from "../messages/Error";
 
 const Input = forwardRef(function Input(
-  { label, type = "text", className = "", ...props },
+  { label, type = "text", errorMessage="", isError, required = false, className = "", ...props },
   ref
 ) {
   const id = useId();
   return (
-    <div className="w-full flex flex-col gap-2">
-      {label && (
-        <label className="" htmlFor={id}>
-          {label}
-        </label>
-      )}
+    <>
+      <div className="w-full flex flex-col gap-2">
+        {label && (
+          <label className="" htmlFor={id}>
+            {label} {required && <span className="text-red-400">*</span>}
+          </label>
+        )}
 
-      <input type={type} className={`outline-none text-md px-3 pr-10 py-2 rounded-md w-full focus:border-gray-500 text-gray-100 h-10 bg-gray-700 border border-gray-600 ${className}`} ref={ref} id={id} {...props}  />
-    </div>
+        <input
+          type={type}
+          className={`outline-none text-md px-3 pr-10 py-2 rounded-md w-full focus:border-gray-500 text-gray-100 h-10 bg-gray-700 border border-gray-600 ${className}`}
+          ref={ref}
+          id={id}
+          {...props}
+        />
+      </div>
+      <div className=" h-5">{errorMessage && <Error message={errorMessage} />}</div>
+    </>
   );
 });
 
