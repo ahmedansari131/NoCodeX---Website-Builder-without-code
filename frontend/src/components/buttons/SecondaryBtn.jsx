0bbdeca4de10641963502f4b8e77418a458894ae
Ahmed Ansari <ansarimohammadahmed49@gmail.com>
2023-10-10 21:03:16 +0530

    Buttons created

--- frontend/src/components/buttons/SecondaryBtn.jsx
+++ frontend/src/components/buttons/SecondaryBtn.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const SecondaryBtn = ({ text, type = "button", className="", ...props }) => {
   return (
-    <button type={type} className={`border-teal-500 border w-full px-4 py-2 text-lg rounded-md text-white font-medium transition-all duration-200 hover:bg-teal-900  ${className}`} {...props}>
+    <button type={type} className={`border-teal-500 border w-full px-4 py-2 text-lg rounded-md text-white font-medium transition-all duration-200 hover:bg-teal-900 ${className}`} {...props}>
       {text}
     </button>
   );
--- frontend/src/components/buttons/TertiaryBtn.jsx
+++ frontend/src/components/buttons/TertiaryBtn.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const TertiaryBtn = ({ text, type = "button", className = "", ...props }) => {
+const TertiaryBtn = ({ text, type = "button", className = "",icon=null, ...props }) => {
   return (
     <button
       type={type}
@@ -8,6 +8,7 @@ const TertiaryBtn = ({ text, type = "button", className = "", ...props }) => {
       className={`py-2 text-lg rounded-md text-white font-medium transition-all duration-200 hover:text-teal-500 ${className}`}
     >
       {text}
+      {icon}
     </button>
   );
 };
