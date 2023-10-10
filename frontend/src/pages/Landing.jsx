e15cb17553e770b8b8f5a806c146b196fcf5d8db
Ahmed Ansari <ansarimohammadahmed49@gmail.com>
2023-10-10 21:07:19 +0530

    Added buttons for github and get started

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -1,17 +1,11 @@
 import React from "react";
-import {
-  Header,
-  Footer,
-  Input,
-  PrimaryBtn,
-  SecondaryBtn,
-} from "../components/index.js";
-import Login from "./login/Login.jsx";
+import { PrimaryBtn, TertiaryBtn } from "../components/index.js";
+import LaunchIcon from '@mui/icons-material/Launch';
 
 const Landing = () => {
   return (
     <>
-      <div className="bg-slate-900 w-full h-screen text-white flex justify-center items-center flex-col gap-8">
+      <div className="bg-slate-900 w-full py-56 text-white flex justify-center items-center flex-col gap-8">
         <div className="w-2/3 flex flex-col gap-6 justify-center items-center">
           <h1 className="text-6xl font-bold text-center  ">
             Unleash the creativity with the furture of web development.
@@ -21,8 +15,14 @@ const Landing = () => {
             coding required. Transform your ideas into reality today!
           </p>
         </div>
-        <div>
-          <PrimaryBtn text="Get Started" />
+        <div className="flex gap-12 items-center">
+          <div>
+            <PrimaryBtn text="Get Started" />
+          </div>
+          <div>
+            <a href="https://github.com/ahmedansari131/NoCodeX---Website-Builder-without-code" target="_blank">
+            <TertiaryBtn className="hover:underline flex items-center gap-2" icon={<LaunchIcon style={{fontSize: "1.4rem"}}  />} text="Github" /></a>
+          </div>
         </div>
       </div>
     </>
