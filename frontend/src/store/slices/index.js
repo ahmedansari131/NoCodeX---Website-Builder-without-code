b458fc5e21a03b17bff38a3567b4c367894619ee
Ahmed Ansari <ansarimohammadahmed49@gmail.com>
2023-10-10 20:45:50 +0530

    Imports added for exportation

---
+++ frontend/src/store/slices/index.js
@@ -0,0 +1,11 @@
+import profileSlice from "./userProfile/profileSlice";
+import { setUserProfile } from "./userProfile/profileSlice";
+import registrationSlice from "./Registration/registrationSlice";
+import { setRegistered } from "./Registration/registrationSlice";
+
+export {
+    profileSlice,
+    setUserProfile,
+    setRegistered,
+    registrationSlice
+}
