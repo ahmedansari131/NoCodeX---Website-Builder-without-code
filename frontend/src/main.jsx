5d6c07dec13366bad7135f7e10bdd7c87b5ec3f9
Ahmed Ansari <ansarimohammadahmed49@gmail.com>
2023-10-10 21:12:28 +0530

    Removed routes and added to the App

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,33 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import App1 from "./App.jsx";
 import "./index.css";
-import {
-  Route,
-  RouterProvider,
-  createBrowserRouter,
-  createRoutesFromElements,
-} from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./store/store.js";
-import { Login, Landing, Signup, EmailVerification } from "./pages/index.js";
-import { PrivateRoutes } from "./components/index.js";
-
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<App />}>
-      <Route path="/" element={<Landing />} />
-      <Route path="signup" element={<Signup />} />
-      <Route path="verify-email" element={<PrivateRoutes VerifyEmail={EmailVerification} /> } />
-      <Route path="login" element={<Login />} />
-    </Route>
-  )
-);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <RouterProvider router={router} />
-    </Provider>
-  </React.StrictMode>
+  // <React.StrictMode>
+  <Provider store={store}>
+    <App1 />
+  </Provider>
+  // {/* </React.StrictMode> */}
 );
