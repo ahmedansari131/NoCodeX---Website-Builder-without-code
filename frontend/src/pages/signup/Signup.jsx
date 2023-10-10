a2b892badb1beebb16aa56e65a1e6a1083e89147
Ahmed Ansari <ansarimohammadahmed49@gmail.com>
2023-10-10 21:08:37 +0530

    Signup page created

--- frontend/src/pages/signup/Signup.jsx
+++ frontend/src/pages/signup/Signup.jsx
@@ -1,49 +1,101 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import {
   Input,
   Loader,
   PrimaryBtn,
   SecondaryBtn,
+  Success,
+  Error,
 } from "../../components/index";
-import { useRegisterUserMutation } from "../../services/authApi";
+import { useRegisterUserMutation } from "../../services/index";
 import GoogleIcon from "@mui/icons-material/Google";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
+import { useDispatch } from "react-redux";
+import { setRegistered } from "../../store/slices";
 
 const Signup = () => {
   const [registerUser, { isLoading, isError }] = useRegisterUserMutation();
-  const nameRef = useRef();
-  const emailRef = useRef();
-  const passRef = useRef();
-  const cPassRef = useRef();
+  const [nameError, setNameError] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [passError, setPassError] = useState("");
+  const [cPassError, setCPassError] = useState("");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [cPassword, setCPassword] = useState("");
+  const [passFocus, setPassFocus] = useState(false);
+  const [cPassFocus, setCPassFocus] = useState(false);
+  const [passInputType, setPassInputType] = useState("password");
+  const [cPassInputType, setCPassInputType] = useState("password");
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const passRef = useRef(null);
+  const cPassRef = useRef(null);
+
+  const passInputHandler = (e) => {
+    const content = passRef.current.value;
+    setPassword(content);
+    setPassFocus(true);
+    if (!content) setPassFocus(false);
+  };
+
+  const cPassInputHandler = (e) => {
+    const content = cPassRef.current.value;
+    setCPassword(content);
+    setCPassFocus(true);
+    if (!content) setCPassFocus(false);
+  };
 
   const handleSignup = async (e) => {
     e.preventDefault();
-    const name = nameRef.current.value;
-    const email = emailRef.current.value;
-    const password = passRef.current.value;
-    const confirmPassword = cPassRef.current.value;
 
     const registerObj = {
       name: name,
       email: email,
       password: password,
-      confirmPassword: confirmPassword,
+      password2: cPassword,
     };
 
-    nameRef.current.value = "";
-    emailRef.current.value = "";
-    passRef.current.value = "";
-    cPassRef.current.value = "";
-
     try {
-      let response = await registerUser(registerObj);
-      const signupData = {
-        email: response.data["data"]["email"],
-        isRegisterSuccess: true,
-      };
-      localStorage.setItem("registration", JSON.stringify(signupData));
-      navigate("/verify-email");
+      setNameError("");
+      setEmailError("");
+      setPassError("");
+      setCPassError("");
+      const response = await registerUser(registerObj);
+      if (response.data["status"] === 400) {
+        if (response.data.message.email) {
+          console.log("name");
+          setEmailError(response.data.message.email[0]);
+        }
+        if (response.data.message.name) {
+          setNameError(response.data.message.name[0]);
+        }
+        if (response.data.message.password) {
+          setPassError(response.data.message.password[0]);
+        }
+        if (response.data.message.password2) {
+          setCPassError(response.data.message.password2[0]);
+        }
+      }
+      if (response.data["status"] === 200) {
+        setName("");
+        setEmail("");
+        setPassword("");
+        setCPassword("");
+        const signupData = {
+          email: response.data["data"]["email"],
+          isRegisterSuccess: true,
+        };
+        localStorage.setItem("registration", JSON.stringify(signupData));
+        dispatch(
+          setRegistered(JSON.parse(localStorage.getItem("registration"))?.isRegisterSuccess)
+        );
+        navigate("/verify-email");
+      } else {
+        console.log(response.data);
+      }
     } catch (error) {
       console.log("Error occurred while registering the user ", error);
     }
@@ -54,28 +106,108 @@ const Signup = () => {
       <div className="bg-gray-900 text-white flex justify-center items-center p-20 px-96">
         <div className="bg-gray-800 rounded-lg flex items-center overflow-hidden h-fit w-full">
           <div className="w-full h-full flex items-center justify-center px-14 py-12">
-            <form className="flex flex-col gap-5 w-full">
+            <form className="flex flex-col gap-2 w-full">
               <h2 className="mb-8 font-bold uppercase text-4xl text-gray-300 relative">
                 Signup
               </h2>
-              <div className="flex gap-6 w-full">
+              <div className="flex gap-8 w-full">
                 <div className="flex flex-col gap-1 text-sm text-gray-400 w-full">
-                  <Input label="Name" ref={nameRef} required />
+                  <Input
+                    required={true}
+                    label={`Name`}
+                    errorMessage={nameError && (!name || name) ? nameError : ""}
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    className={`${
+                      nameError && (!name || name) ? "border-red-400" : ""
+                    }`}
+                  />
                 </div>
                 <div className="flex flex-col gap-1 text-sm text-gray-400 w-full">
-                  <Input label="Email" ref={emailRef} required />
+                  <Input
+                    required={true}
+                    label="Email"
+                    errorMessage={
+                      emailError && (!email || email) ? emailError : ""
+                    }
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    className={`${
+                      emailError && (!email || email) ? "border-red-400" : ""
+                    }`}
+                  />
                 </div>
               </div>
 
-              <div className="flex gap-6 w-full">
-                <div className="flex flex-col gap-1 text-sm text-gray-400 w-full">
-                  <Input label="Password" type="password" ref={passRef} />
-                </div>
-                <div className=" flex flex-col gap-1 text-sm text-gray-400 w-full">
+              <div className="flex gap-8 w-full">
+                <div
+                  className={`flex flex-col gap-1 text-sm text-gray-400 w-full relative }`}
+                >
+                  {passFocus && passInputType === "password" && (
+                    <VisibilityOffIcon
+                      className="absolute top-1/2 right-4 -translate-y-2"
+                      style={{ fontSize: "1.1rem" }}
+                      onClick={() => setPassInputType("text")}
+                    />
+                  )}
+
+                  {passFocus && passInputType === "text" && (
+                    <VisibilityIcon
+                      className="absolute top-1/2 right-4 -translate-y-2"
+                      style={{ fontSize: "1.1rem" }}
+                      onClick={() => setPassInputType("password")}
+                    />
+                  )}
                   <Input
-                    label="Confirm Password"
-                    type="password"
+                    required={true}
+                    ref={passRef}
+                    type={passInputType}
+                    label="Password"
+                    errorMessage={
+                      passError && (!password || passError) ? passError : ""
+                    }
+                    value={password}
+                    onChange={(e) => {
+                      passInputHandler(e);
+                    }}
+                    className={`${
+                      passError && (!password || password)
+                        ? "border-red-400"
+                        : ""
+                    }`}
+                  />
+                </div>
+                <div className=" flex flex-col gap-1 text-sm text-gray-400 w-full relative">
+                  {cPassFocus && cPassInputType === "password" && (
+                    <VisibilityOffIcon
+                      className="absolute top-1/2 right-4 -translate-y-2"
+                      style={{ fontSize: "1.1rem" }}
+                      onClick={() => setCPassInputType("text")}
+                    />
+                  )}
+
+                  {cPassFocus && cPassInputType === "text" && (
+                    <VisibilityIcon
+                      className="absolute top-1/2 right-4 -translate-y-2"
+                      style={{ fontSize: "1.1rem" }}
+                      onClick={() => setCPassInputType("password")}
+                    />
+                  )}
+                  <Input
+                    required={true}
                     ref={cPassRef}
+                    label="Confirm Password"
+                    type={cPassInputType}
+                    errorMessage={cPassError && !cPassword ? cPassError : ""}
+                    value={cPassword}
+                    onChange={(e) => {
+                      cPassInputHandler(e);
+                    }}
+                    className={`${
+                      cPassError && (!cPassword || cPassword)
+                        ? "border-red-400"
+                        : ""
+                    }`}
                   />
                 </div>
               </div>
