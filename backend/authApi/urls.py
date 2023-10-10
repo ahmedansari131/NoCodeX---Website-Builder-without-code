ab5f2b82e46e2303a8447410e103b25c99aafd8a
Ahmed Ansari <ansarimohammadahmed49@gmail.com>
2023-10-10 21:17:24 +0530

    Apis urls of login, registration, verification, profile added

--- backend/authApi/urls.py
+++ backend/authApi/urls.py
@@ -1,7 +1,17 @@
 from django.urls import path
 from .views import *
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 
 urlpatterns = [
-    path('register/', RegisterApi.as_view(), name="Registeration"),
-    path('verify-otp/', VerifyOtp.as_view(), name="OtpVerification"),
-]
+    path("register/", RegisterApi.as_view(), name="Registeration"),
+    path("verify-otp/", VerifyOtp.as_view(), name="OtpVerification"),
+    path("login/", LoginApi.as_view(), name="LoginUser"),
+    path("user-profile-setup/", ProfileApi.as_view(), name="UserProfile"),
+    path("user-profile-setup/<int:id>", ProfileApi.as_view(), name="UserProfileUpdate"),
+    path("refresh-token/", TokenRefreshView.as_view(), name="TokenRefresh"),
+] 
+
