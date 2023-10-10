bcd5be2e3d4b261cfb276073e5e1626848d9851f
Ahmed Ansari <ansarimohammadahmed49@gmail.com>
2023-10-10 21:07:51 +0530

    Added imports of all the pages for exportation

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -2,10 +2,14 @@ import Login from "./login/Login"
 import Landing from "./Landing"
 import Signup from "./signup/Signup"
 import EmailVerification from "./verification/EmailVerification"
+import ProfileSetup from "./profile/ProfileSetup"
+import Error404 from "./Error/Error404"
 
 export {
     Login,
     Landing,
     Signup,
     EmailVerification,
+    ProfileSetup,
+    Error404,
 }
